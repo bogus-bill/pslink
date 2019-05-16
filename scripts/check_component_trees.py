ae2debbda73903769c57848552810e69ba43e3b2
msrocka <michael.srocka@gmail.com>
2019-05-16 14:57:26 +0200

    finish component check

--- scripts/check_component_trees.py
+++ scripts/check_component_trees.py
@@ -1,13 +1,23 @@
+# check the consistency of the Excel files with the component trees: compare
+# the tree structure which results from the level column with the parent
+# pointers (there were inconsistencies in some sheets)
+
+import glob
 import xlrd
 
 
 def main():
-    files = ["MLG F18EFG ABL_c.xlsx", "NLG F18EFG ABL_c.xlsx"]
-    for f in files:
-        wb = xlrd.open_workbook("../data/" + f)
-        print("Check file", f)
+    file_count = 0
+    sheet_count = 0
+    error_count = 0
+
+    for f in glob.glob("../data/*.xlsx"):
+        file_count += 1
+        print("check component trees in", f)
+        wb = xlrd.open_workbook(f)
         for sheet_name in wb.sheet_names():
-            print("  Check sheet", sheet_name)
+            sheet_count += 1
+            print("  check sheet", sheet_name)
             sheet = wb.sheet_by_name(sheet_name)
 
             stack = []
@@ -23,9 +33,13 @@ def main():
                 if level > 0:
                     parent = _cell_str(sheet, r, 5)
                     if stack[level - 1] != parent:
+                        error_count += 1
                         print("    err: row %i next=%s but parent=%s" % (
                             r, parent, stack[level - 1]))
                 stack.append(part_number)
+
+    print("scanned materials in %i files with %i sheets: found %i errors" %
+          (file_count, sheet_count, error_count))
 
 
 def _cell_str(sheet, row, col) -> str:
